e082928b3aa099b310f316ef81dee057dc01407c
paulslugocki <paulslugocki@gmail.com>
2015-02-04 20:31:08 +0000

    Initial commit

---
+++ node-tourcms.js
@@ -0,0 +1,481 @@
+var https = require('https');
+var crypto = require('crypto');
+var querystring = require('querystring');
+
+var concat = require('concat-stream');
+var xml2js = require('xml2js');
+
+// Constructor
+
+function TourCMS(config) {
+
+// Initialising instance properties
+this.hostname = 'api.tourcms.com';
+this.apiKey = '';
+this.marketplaceId = 0;
+this.channelId = 0;
+this.channels = [];
+
+// Process config
+if(typeof config.apiKey !== 'undefined')
+  this.apiKey = config.apiKey;
+
+if(typeof config.marketplaceId !== 'undefined')
+  this.marketplaceId = config.marketplaceId;
+
+if(typeof config.channelId !== 'undefined')
+  this.channelId = config.channelId;
+
+}
+
+// class methods
+
+TourCMS.prototype.makeRequest = function(a) {
+
+// Sensible defaults
+if(typeof a.channelId == "undefined")
+  a.channelId = 0;
+
+if(typeof a.verb == "undefined")
+  a.verb = 'GET';
+
+if(typeof a.postData == "undefined") {
+  var apiParams = "";
+} else {
+  // Convert object into XML
+  var builder = new xml2js.Builder();
+  var apiParams = builder.buildObject(a.postData);
+}
+
+// Get the current time
+var outboundTime = this.generateTime();
+
+// Generate the signature
+var signature = this.generateSignature(a.path, a.channelId, a.verb, outboundTime, this.apiKey);
+
+var options = {
+  method: a.verb,
+  hostname: this.hostname,
+  path: a.path,
+  headers: {
+    'x-tourcms-date': outboundTime,
+    'Authorization': 'TourCMS ' + a.channelId + ':' + this.marketplaceId + ':' + signature,
+    'Content-type': 'text/xml;charset="utf-8"',
+    'Content-length': apiParams.length
+  }
+}
+
+var req = https.request(options, function(response) {
+
+  response.setEncoding("utf8");
+
+  // Pipe to handler
+  var concatStream = concat(apiResponded);
+  response.pipe(concatStream);
+
+  // TODO: Handle errors
+  response.on("error", console.log);
+
+  // Process API response
+  function apiResponded(apiResponse) {
+
+    // Convert XML to JS object
+    var parser = new xml2js.Parser({explicitArray:false});
+    parser.parseString(apiResponse, function (err, result) {
+      // If the method processes the response, give it back
+      // Otherwise call the original callback
+
+      if(typeof a.processor !== 'undefined')
+        a.processor(result.response, a.callback);
+      else
+        a.callback(result.response);
+    });
+
+
+  }
+
+});
+
+// If we're posting, let's post
+if(a.verb == "POST")
+  req.write(apiParams);
+
+// We're done requesting
+req.end();
+};
+
+// Housekeeping
+
+TourCMS.prototype.apiRateLimitStatus = function(a) {
+
+if(typeof a === 'undefined')
+a = {};
+
+if(typeof a.channelId === 'undefined')
+  a.channelId = this.channelId;
+
+
+  a.path = '/api/rate_limit_status.xml';
+
+  this.makeRequest(a);
+
+};
+
+// Channels
+
+TourCMS.prototype.listChannels = function(a) {
+
+  if(typeof a === 'undefined')
+    a = {};
+
+  a.path = '/p/channels/list.xml';
+
+  this.makeRequest(a);
+};
+
+TourCMS.prototype.showChannel = function(a) {
+
+    if(typeof a === 'undefined')
+      a = {};
+
+    if(typeof a.channelId === 'undefined')
+      a.channelId = this.channelId;
+
+    a.path = '/c/channel/show.xml';
+
+    this.makeRequest(a);
+};
+
+TourCMS.prototype.channelPerformance = function(a) {
+
+    if(typeof a === 'undefined')
+      a = {};
+
+    if(typeof a.channelId === 'undefined')
+      a.channelId = this.channelId;
+
+    a.path = '/p/channels/performance.xml';
+
+    this.makeRequest(a);
+}
+
+// Tours
+
+// Seach Tours
+TourCMS.prototype.searchTours = function(a) {
+
+  if(typeof a === 'undefined')
+    a = {};
+
+  // Convert/set search params
+  // If undefined
+  if(typeof a.qs === "undefined")
+    a.qs = {};
+
+  a.qs = querystring.stringify(a.qs);
+
+  // Channel ID
+  // If undefined, use object level channelId
+  if(typeof a.channelId === "undefined")
+    a.channelId = this.channelId;
+
+  // Set API path
+  if(a.channelId==0)
+    a.path = '/p/tours/search.xml?' + a.qs;
+  else
+    a.path = '/c/tours/search.xml?' + a.qs;
+
+  // Sanitise response, total_tour_count always set
+  // Tours is an array if empty
+
+  a.processor = function(response, callback) {
+
+    // Ensure we have a total tour count
+    if(typeof response.total_tour_count === 'undefined')
+      response.total_tour_count = '0';
+
+    // Ensure we have an array of tours
+    response.tour = [].concat[response.tour];
+
+    callback(response);
+  }
+
+  this.makeRequest(a);
+
+};
+
+// List Tours
+TourCMS.prototype.listTours = function(a) {
+
+  if(typeof a === 'undefined')
+    a = {};
+
+  // Convert/set search params
+  // If undefined
+  if(typeof a.qs === "undefined")
+  a.qs = {};
+
+  a.qs = querystring.stringify(a.qs);
+
+  // Channel ID
+  // If undefined, use object level channelId
+  if(typeof a.channelId === "undefined")
+    a.channelId = this.channelId;
+
+  // Set API path
+  if(a.channelId==0)
+    a.path = '/p/tours/list.xml?' + a.qs;
+  else
+    a.path = '/c/tours/list.xml?' + a.qs;
+
+  // Sanitise response, tours is an array if empty
+  a.processor = function(response, callback) {
+
+    // Ensure we have an array of tours
+    response.tour = [].concat[response.tour];
+
+    callback(response);
+
+  }
+
+  this.makeRequest(a);
+};
+
+// Show Tour
+TourCMS.prototype.showTour = function(a) {
+
+  // If QS undefined
+  if(typeof a.qs === "undefined") {
+    a.qs = {};
+  }
+
+  // Add in the TourId in if provided separately
+  if(typeof a.tourId !== 'undefined') {
+    a.qs['id'] = a.tourId;
+
+  }
+
+  // Fix id if passed in to qs directly as tourId
+  if(typeof a.qs.tourId !== 'undefined') {
+    a.qs['id'] = a.qs.tourId;
+    delete a.qs['tourId'];
+  }
+
+  a.qs = querystring.stringify(a.qs);
+
+  // Channel ID
+  // If undefined, use object level channelId
+  if(typeof a.channelId === "undefined")
+    a.channelId = this.channelId;
+
+  a.path = '/c/tour/show.xml?' + a.qs;
+
+  this.makeRequest(a);
+};
+
+
+// Update Tour
+TourCMS.prototype.updateTour = function(a) {
+
+  // Channel ID
+  // If undefined, use object level channelId
+  if(typeof a.channelId === "undefined")
+    a.channelId = this.channelId;
+
+  // Tour ID
+  // If tour_id is provided as tourId, fix
+  if(typeof a.tour.tour_id === "undefined") {
+    if(typeof a.tour.tourId !== "undefined") {
+      a.tour.tour_id = a.tour.tourId;
+      delete a.tour.tourId;
+    }
+  }
+
+  // Set post data
+  a.postData = {tour: a.tour};
+
+  // Set API path
+  a.path = '/c/tour/update.xml';
+
+  a.verb = 'POST';
+
+  this.makeRequest(a);
+};
+
+TourCMS.prototype.showTourDatesDeals = function(a) {
+
+  // If QS undefined
+  if(typeof a.qs === "undefined")
+    a.qs = {};
+
+  // Add in the TourId in if provided separately
+  if(typeof a.tourId !== 'undefined')
+    a.qs['id'] = a.tourId;
+
+  // Fix id if passed in to qs directly as tourId
+  if(typeof a.qs.tourId !== 'undefined') {
+    a.qs['id'] = a.qs.tourId;
+    delete a.qs['tourId'];
+  }
+
+  a.qs = querystring.stringify(a.qs);
+
+  // Channel ID
+  // If undefined, use object level channelId
+  if(typeof a.channelId === "undefined")
+    a.channelId = this.channelId;
+
+  a.path = '/c/tour/datesprices/datesndeals/search.xml?' + a.qs;
+
+  // Sanitise response, total_date_count always set
+  // Tours is an array if empty
+
+  a.processor = function(response, callback) {
+
+  // Ensure we have a total_date_count
+  if(typeof response.total_date_count === 'undefined')
+    response.total_date_count = '0';
+
+  // Ensure we have an array of dates
+  if(typeof response.dates_and_prices === 'undefined')
+    response.dates_and_prices = {date:[]};
+  else
+    response.dates_and_prices.date = [].concat(response.dates_and_prices.date);
+
+    callback(response);
+  }
+
+  this.makeRequest(a);
+};
+
+// Check Tour Availability
+TourCMS.prototype.checkTourAvailability = function(a) {
+
+  // If QS undefined
+  if(typeof a.qs === "undefined") {
+    a.qs = {};
+  }
+
+  // Add in the TourId in if provided separately
+  if(typeof a.tourId !== 'undefined') {
+    a.qs['id'] = a.tourId;
+  }
+
+  // Fix id if passed in to qs directly as tourId
+  if(typeof a.qs.tourId !== 'undefined') {
+    a.qs['id'] = a.qs.tourId;
+    delete a.qs['tourId'];
+  }
+
+  a.qs = querystring.stringify(a.qs);
+
+  // Channel ID
+  // If undefined, use object level channelId
+  if(typeof a.channelId === "undefined")
+    a.channelId = this.channelId;
+
+  // Sanitise response
+  // Ensure we have an available_components.component array
+  a.processor = function(response, callback) {
+
+    if(response.available_components == '\r\n')
+      response.available_components = {component:[]};
+    else
+      response.available_components.component = [].concat(response.available_components.component);
+
+    // Ensure each components contents are correct
+    response.available_components.component.forEach(function(component) {
+      // Price rows
+      component.price_breakdown.price_row = [].concat(component.price_breakdown.price_row);
+      // Pickups
+      if(typeof component.options === 'undefined')
+        component.options = {option:[]};
+      else
+        component.options.option = [].concat(component.options.option);
+      // Options
+      if(typeof component.pickup_points === 'undefined')
+          component.pickup_points = {pickup:[]};
+        else
+          component.pickup_points.pickup = [].concat(component.pickup_points.pickup);
+    });
+
+    callback(response);
+  };
+
+  a.path = '/c/tour/datesprices/checkavail.xml?' + a.qs;
+
+  this.makeRequest(a);
+};
+
+
+// Bookings
+
+TourCMS.prototype.searchVouchers = function(a) {
+
+  // Channel ID
+  // If undefined, use object level channelId
+  if(typeof a.channelId === "undefined")
+    a.channelId = channelId;
+
+  // Voucher string to search
+  if(typeof a.voucherString === "undefined")
+    a.voucherString = '';
+
+  // Build object that will be turned into XML
+  a.postData = ({
+    voucher: {
+      barcode_data: a.voucherString
+    }
+  });
+
+  // If we're searching wide dates, add that on
+  if(typeof a.wideDates != 'undefined') {
+    a.postData.voucher.wide_dates = a.wideDates;
+  }
+
+  // Set API path
+  if(a.channelId==0)
+    a.path = '/p/voucher/search.xml';
+  else
+    a.path = '/c/voucher/search.xml';
+
+  // POST
+  a.verb = 'POST';
+
+  /*
+  a.processor = function(data, callback) {
+    callback(data);
+  }*/
+
+  this.makeRequest(a);
+}
+
+TourCMS.prototype.generateSignature = function(path, channelId, verb, outboundTime, apiKey) {
+
+  var stringToSign = channelId + "/" + this.marketplaceId + "/" + verb + "/" + outboundTime + path;
+  var hash = crypto.createHmac('SHA256', apiKey).update(stringToSign).digest('base64');
+  var signature = this.rawurlencode(hash);
+
+  return signature;
+};
+
+// Generate the current Unix Timestamp (PHP style)
+TourCMS.prototype.generateTime = function() {
+  return Math.floor(new Date().getTime() / 1000);
+}
+
+// URL encode to match PHP
+TourCMS.prototype.rawurlencode = function(str) {
+  str = (str + '').toString();
+
+  return encodeURIComponent(str)
+  .replace(/!/g, '%21')
+  .replace(/'/g, '%27')
+  .replace(/\(/g, '%28')
+  .replace(/\)/g, '%29')
+  .replace(/\*/g, '%2A');
+};
+
+// export the class
+
+module.exports = TourCMS;
---
+++ readme.md
@@ -0,0 +1,197 @@
+# node-tourcms
+
+Node wrapper for the [TourCMS](http://www.tourcms.com/) [API](http://www.tourcms.com/support/api/mp/).
+
+## Installation
+
+The easiest way to install is via [npm](http://npmjs.org) which will install node-tourcms and all dependencies.
+
+```js
+npm install tourcms
+```
+
+## Usage
+
+Require `tourcms`
+
+```js
+var TourCMSApi = require('tourcms');
+```
+Create a new `tourcms` API object, passing in your API credentials
+```js
+var TourCMS = new TourCMSApi({
+    channelId: YOUR_CHANNEL_ID,
+    apiKey: 'YOUR_API_KEY',
+    marketplaceId: YOUR_MARKETPLACE_ID
+  });
+```
+Then call one of the API methods below, the wrapper uses [xml2js](https://github.com/Leonidas-from-XIV/node-xml2js) to automatically convert the response XML into a JavaScript object, some effort is also made to ensure things that might be arrays, are.
+
+## API methods
+
+### General / Housekeeping APIs
+
+#### API Rate Limit Status
+
+Check the current API rate limit status (limit is 2000 requests per channel per hour at the time of writing this readme)
+
+http://www.tourcms.com/support/api/mp/rate_limit_status.php
+
+```js
+TourCMS.apiRateLimitStatus({
+    channelId: 3930,
+    callback: function(response) {
+      console.log(response);
+    }
+  })
+```
+
+### Channel APIs
+
+#### List Channels
+
+List channels.
+
+http://www.tourcms.com/support/api/mp/channels_list.php
+
+```js
+TourCMS.listChannels({
+  callback: function(response) {
+      console.log(response);
+  }
+})
+```
+
+#### Show Channel
+
+Show details on a specific channel.
+
+If a Channel ID is not provided, the function will use the Channel ID passed in the initial configuration.
+
+http://www.tourcms.com/support/api/mp/channel_show.php
+
+```js
+TourCMS.showChannel({
+  channelId: 3930,
+  callback: function(response) {
+    console.log(response);
+  }
+})
+```
+
+#### Channel Performance
+
+List top 50 Channels by number of unique visitor clicks (or check performance for a specific channel).
+
+Optonally supply a `channelId` to just return a specific Channel.
+
+http://www.tourcms.com/support/api/mp/channels_performance.php
+
+```js
+TourCMS.channelPerformance({
+  channelId: 3930,
+  callback: function(response) {
+    console.log(response);
+  }
+});
+```
+
+### Tour (Product) APIs
+#### Search Tours
+
+Search Tours.
+
+If a Channel ID is not provided, the function will use the Channel ID passed in the initial configuration.
+
+http://www.tourcms.com/support/api/mp/tour_search.php
+
+```js
+TourCMS.searchTours({
+  channelId: 3930,
+  qs: {
+    k: 'rafting',
+    order: 'price_down'
+  },
+  callback: function(response) {
+    console.log(response);
+  }
+});
+```
+
+#### List Tours
+
+List Tours, quicker and not pagenated versus Search Tours, however returns less data. Ideal for exporting from TourCMS.
+
+If a Channel ID is not provided, the function will use the Channel ID passed in the initial configuration.
+
+http://www.tourcms.com/support/api/mp/tour_list.php
+
+```js
+TourCMS.listTours({
+  channelId: 3930,
+  callback: function(response) {
+    console.log(response);
+  }
+});
+```
+
+#### Show Tour
+
+Return details on a specific Tour.
+
+If a `channelId` is not provided, the one passed in the initial configuration will be used.
+
+http://www.tourcms.com/support/api/mp/tour_show.php
+
+```js
+TourCMS.showTour({
+  tourId: 1,
+  callback: function(response) {
+    console.log(response);
+  }
+});
+```
+
+#### Update Tour
+
+Update tour, currently supports updating the `tour_url` only.
+
+If a `channelId` is not provided, the one passed in the initial configuration will be used.
+
+http://www.tourcms.com/support/api/mp/tour_update.php
+
+```js
+TourCMS.showTour({
+  tour: {
+    tourId: 1,
+    tour_url: '/tours/example_tour_1/'
+  },
+  callback: function(response) {
+    console.log(response);
+  }
+});
+```
+
+#### Show Tour Dates and Deals
+List the dates available for a specific tour.
+
+If a `channelId` is not provided, the one passed in the initial configuration will be used.
+
+http://www.tourcms.com/support/api/mp/tour_datesdeals_show.php
+
+```js
+TourCMS.showTourDatesDeals({
+  channelId: 3930,
+  tourId: 1,
+  qs: {
+    has_offer: 1,
+    distinct_start_dates: 1
+  },
+  callback: function(response) {
+    // Loop through and output each component
+    response.available_components.component.forEach(function(item) {
+      console.log(item);
+    });
+  }
+});
+```
